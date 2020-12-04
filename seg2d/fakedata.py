2a9a11cd72ebcc9964b41a0a4f8b8eb54257d02e
michi-d <drews@neuro.mpg.de>
2020-12-04 10:41:08 +0100

    add vis3d and fakedata2d

---
+++ seg2d/fakedata.py
@@ -0,0 +1,344 @@
+"""
+Modulate for creation of fake data in 2D.
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import time
+from scipy.linalg import norm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def clean_mask_out_of_bounds(A, bound=128):
+    """Deletes all points with any coordinate larger than a bound.
+    
+    Args:
+        A: input array
+        bound: bound
+    
+    Returns:
+        mask: boolean mask for points within the bounds
+    """
+    mask_low = ~(A<0).any(axis=1)
+    mask_high = ~(A>=bound).any(axis=1)
+    return mask_low & mask_high
+
+
+class Point():    
+    """Defines a point in 2D space with an intensity value.
+    """
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+        
+class Dendrite():
+    """Defines a dendrite as a cloud of points.
+    """
+    
+    def __init__(self, x0, y0, intensity, n_points, std_x, std_y, std_v, 
+                 ring_d, ring_std, mode='ring'):
+        """Generator method.
+        
+        Args:
+            x0: x-coordinate center point
+            y0: y-coordinate center point
+            intensity: intensity of the dendrite
+            n_points: how many points in the cloud
+            std_x: standard deviation in x (for "gaussian" mode)
+            std_y: standard deviation in y (for "gaussian" mode)
+            std_v: standard deviation of the intensity
+            ring_d: distance ring (for "ring" mode)
+            ring_std: standard deviation in distance from ring (for "ring" mode)
+            mode: "ring" or "gaussian"
+        """
+        
+        self.points = []
+        center_point = Point(x0, y0, intensity)
+        self.points.append(center_point)
+        
+        self.x0 = x0
+        self.y0 = y0
+        self.intensity = intensity
+        self.std_x = std_x
+        self.std_y = std_y
+        self.std_v = std_v
+        self.n_points = n_points
+        self.ring_d = ring_d
+        self.ring_std = ring_std
+        self.mode = mode
+        
+    def grow(self):
+        """Generate point cloud around the center point
+        """
+        n_points = self.n_points
+        std_x = self.std_x
+        std_y = self.std_y
+        std_v = self.std_v
+        mode = self.mode
+          
+        if mode == 'ring':
+            r = np.round(np.random.normal(loc=self.ring_d, scale=self.ring_std, size=n_points), 2)
+            phi = np.round(np.random.uniform(low=0, high=360, size=n_points), 2)
+            x = self.x0 + (np.cos(phi)*r).astype(np.int)
+            y = self.y0 + (np.sin(phi)*r).astype(np.int)
+        elif mode == 'gaussian':
+            x = np.round(np.random.normal(loc=self.x0, scale=std_x, size=n_points), 2).astype(np.int)
+            y = np.round(np.random.normal(loc=self.y0, scale=std_y, size=n_points), 2).astype(np.int)
+
+        v = np.random.normal(loc=self.intensity, scale=std_v, size=n_points)
+        
+        xy = np.stack([x, y], axis=1)
+        clean_mask = clean_mask_out_of_bounds(xy)
+        xy = xy[clean_mask, :]
+        v  = v[clean_mask]
+        
+        for i in range(len(xy)):
+            self.points.append(Point(xy[i, 0], xy[i, 1], v[i]))
+            
+            
+class RandomImage():
+    """RandomImage fake data class
+    """
+    
+    def __init__(self, basis_angle=60, d=10, baseline_intensity=50, std_intensity=0.5, n_points=10, 
+                 std_x=2, std_y=2, std_v=5, dendrite_kernel=(2,2), gaussian_kernel=(5,5),
+                 ring_d=3.5, ring_std=0.1, ring_frac=0.5, randomCutOff=True, randomRotation=True,
+                 randomAngles = (-10, 30), randomFraction=True,
+                 randomScaling = (0.9, 1.5), size=(128,128)):
+        """Generator method
+        
+        """
+    
+        self.radius_ground_truth = 3.5
+        
+        self.basis_angle = basis_angle
+        self.d = d
+        self.baseline_intensity = baseline_intensity
+        self.std_intensity = std_intensity
+        self.n_points = n_points
+        self.std_x = std_x
+        self.std_y = std_y
+        self.std_v = std_v
+        self.dendrite_kernel = dendrite_kernel
+        self.gaussian_kernel = gaussian_kernel
+        self.size = size
+        self.ring_d = ring_d
+        self.ring_std = ring_std
+        self.ring_frac = ring_frac
+        self.randomCutOff = randomCutOff
+        self.randomRotation = randomRotation
+        self.randomAngles = randomAngles
+        self.randomFraction = randomFraction
+        self.randomScaling = randomScaling
+        
+        # generate scaling factor
+        scaling_factor = np.random.uniform(*self.randomScaling)
+        
+        # rescale parameters
+        self.scaling_factor = scaling_factor
+        self.d = self.d*self.scaling_factor
+        self.std_x = self.std_x*scaling_factor
+        self.std_y = self.std_y*scaling_factor
+        self.ring_d = self.ring_d*scaling_factor
+        self.ring_std = self.ring_std*scaling_factor
+        self.dendrite_kernel = (np.round(self.dendrite_kernel[0]*scaling_factor, 2).astype('int'),
+                                np.round(self.dendrite_kernel[1]*scaling_factor, 2).astype('int'))
+        self.gaussian_kernel = (self._next_uneven_number(self.gaussian_kernel[0]*scaling_factor),
+                                self._next_uneven_number(self.gaussian_kernel[1]*scaling_factor))
+        self.radius_ground_truth = self.radius_ground_truth*scaling_factor
+        self.n_points = int(self.n_points*scaling_factor)
+        
+        # generate picture
+        self._generate()
+    
+    
+    @staticmethod
+    def _next_uneven_number(i):
+        """Finds next lower uneven number
+        """
+        return int(np.ceil(i/2)*2-1)
+
+
+    def _cut_off_points_randomly(self, points):
+        """Cut off all points above a random line.
+        """
+        
+        # generate random normal vector
+        phi = np.random.uniform(low=0, high=2*np.pi)
+        n = np.array([np.cos(phi), np.sin(phi)])
+        
+        # generate random support vector
+        a = [np.random.uniform(low=0, high=self.size[0]), np.random.uniform(low=0, high=self.size[1])]
+        a = np.array(a)
+        
+        # mask all points on one side of the line
+        proj = np.sum(n * (points-a), axis=1)
+        #print(proj)
+        
+        mask = proj > 0
+        
+        return points[mask]
+    
+    
+    def _rotate_points_randomly(self, points):
+        """Rotate all points randomly
+        """
+        e_0 = np.array([self.size[0]/2., self.size[1]/2.])
+        rot_angle = np.random.uniform(0, 2*np.pi)
+        M = np.array([[np.cos(rot_angle), -np.sin(rot_angle)],
+                      [np.sin(rot_angle), np.cos(rot_angle)]])
+        points = (np.dot(points-e_0, M) + e_0).astype(np.int)
+        return points
+        
+        
+    def _generate(self):
+        """Generates the image
+        """
+
+        # initialize empty image array
+        C = np.zeros(self.size)
+
+        # define grid basis
+        basis_angle = self.basis_angle
+        basis_angle = basis_angle + np.random.uniform(*self.randomAngles)
+        e_1 = np.array([np.sin(np.degrees(0)), np.cos(np.degrees(0))])
+        e_2 = np.array([np.sin(np.radians(basis_angle)), np.cos(np.radians(basis_angle))])
+
+        e_0 = np.array([self.size[0]/2., self.size[1]/2.])
+        X,Y = np.mgrid[-10:10, -10:10]
+
+        # create center points
+        midpoints = (e_0 + X.flatten()[:,np.newaxis] * e_1*self.d + Y.flatten()[:,np.newaxis] * e_2*self.d)
+        midpoints = np.round(midpoints, 2).astype(np.int)
+        
+        # cut-off points randomly
+        if self.randomCutOff:
+            midpoints = self._cut_off_points_randomly(midpoints)
+        
+        # random image rotation
+        if self.randomRotation:
+            midpoints = self._rotate_points_randomly(midpoints)
+            
+        # clean out-of-bounds points
+        midpoints = midpoints[clean_mask_out_of_bounds(midpoints)]
+
+        # generate random ring fraction
+        if self.randomFraction:
+            self.ring_frac = np.random.uniform(0, 1)
+            
+        # generate dendrites
+        dendrites = []
+        for point in midpoints:
+            intensity = np.random.lognormal(mean=1.0, sigma=self.std_intensity, 
+                                            size=(1))*self.baseline_intensity
+            if np.random.uniform(low=0, high=1) > 1 - self.ring_frac:
+                mode = 'ring'
+            else:
+                mode = 'gaussian'
+                
+            d = Dendrite(point[0], point[1], intensity, self.n_points, self.std_x, self.std_y, self.std_v, 
+                         self.ring_d, self.ring_std, mode)
+            d.grow()
+            dendrites.append(d)
+
+        # render points
+        for d in dendrites:
+            for p in d.points:
+                C[p.x, p.y] = p.value
+        kernel = np.ones(self.dendrite_kernel,np.float32)/(self.dendrite_kernel[0]*self.dendrite_kernel[1])
+        
+        # enlarge points
+        C = cv2.filter2D(C, -1, kernel)
+
+        # blur points
+        C = cv2.GaussianBlur(C, self.gaussian_kernel, 0)
+        C[C<0] = 0
+        
+        # neuropil background fluorescence
+        #C = C+C.mean()
+        NF = cv2.GaussianBlur(C, (21,21), 0)
+        C = C + NF + C.mean()
+
+        # poisson noise
+        P = np.zeros_like(C)
+        for i in range(P.shape[0]):
+            for j in range(P.shape[1]):
+                P[i,j] = np.random.poisson(C[i,j])
+
+                
+        # gaussian noise
+        P = P + np.random.normal(loc=0, scale=P.std()/10.0, size=P.shape)
+                
+        self.data = P
+        self.labels = self._generate_ground_truth(midpoints)
+        self.midpoints = midpoints
+        
+        
+    def _generate_ground_truth(self, midpoints):
+        """Generates binary labels.
+        """
+        
+        C = np.zeros(self.size)
+        X,Y = np.mgrid[0:self.size[0], 0:self.size[1]]
+        xy = np.stack([X.flatten(), Y.flatten()], axis=1)
+        difference = (xy[:, :, np.newaxis] - np.array(midpoints).T[np.newaxis, :, :])
+        distances = norm(difference, axis=1)
+        
+        mask = np.any(distances<self.radius_ground_truth, axis=1)
+        mask = np.reshape(mask, self.size)
+        return mask.astype(np.int)
+    
+    
+    def show_data_plus_labels(self, figsize=(15,10), red_crosses=False):
+        """Show image and labels in matplotlib figure.
+        """
+        
+        fig = plt.figure(figsize=figsize)
+
+        ax = plt.subplot(121)
+        im = plt.imshow(self.data)
+        plt.title('data')
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im, cax=cax)
+        if red_crosses:
+            ax.scatter(y=self.midpoints[:,0], x=self.midpoints[:,1], s=25, marker='x', color='r')
+
+        ax = plt.subplot(122)
+        im = plt.imshow(self.labels, alpha = 1)
+        plt.title('labels')
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im, cax=cax)
+        if red_crosses:
+            ax.scatter(y=self.midpoints[:,0], x=self.midpoints[:,1], s=25, marker='x', color='r')
+        
+        return fig
+    
+    
+    def get_parameters(self):
+        """Get generative parameters.
+        """
+        
+        params = {'radius_ground_truth': self.radius_ground_truth,
+                  'basis_angle': self.basis_angle,
+                  'd': self.d, 
+                  'baseline_intensity': self.baseline_intensity,
+                  'std_intensity': self.std_intensity,
+                  'n_points': self.n_points,
+                  'std_x': self.std_x,
+                  'std_y': self.std_y,
+                  'std_v': self.std_v,
+                  'dendrite_kernel': self.dendrite_kernel, 
+                  'gaussian_kernel': self.gaussian_kernel,
+                  'size': self.size, 
+                  'ring_d': self.ring_d, 
+                  'ring_std': self.ring_std, 
+                  'ring_frac': self.ring_frac, 
+                  'scaling_factor': self.scaling_factor}
+        
+        return params
+         
---
+++ seg3d/vis3d.py
@@ -0,0 +1,271 @@
+import ipyvolume as ipv
+import time
+import numpy as np
+from ipywidgets import Layout, Box, VBox, HBox, HTML, Image
+
+
+def get_perspective(fig=None):
+    """Get camera angle and rotation from ipyvolume figure. 
+    
+    Args:
+        fig: target figure
+    
+    Returns:
+        view: camera position
+        rotation: camera angle
+    """
+    
+    if fig is None:
+        fig = ipv.pylab.gcf()
+        
+    view = ipv.pylab.view()
+    rotation = fig.camera.rotation
+    
+    return view, rotation
+
+
+def set_perspective(view=(0,0,0), rotation=(0,0,0, 'XYZ'), perspective=None):
+    """Set camera angle and rotation for ipyvolume figure.
+    
+    Args:
+        view: camera position
+        rotation: camera angle
+    """
+    
+    # set camera position and angle
+    if perspective is None:
+        return 
+    elif perspective == 'oblique':
+        # oblique view
+        view = (-133.60365788878377, 54.701906702426264, 2.4643234833362575)
+        rotation = (-2.0250209294420607, -0.43170845694524784, -0.165007940051523, 'XYZ')
+    elif perspective == 'Y':
+        # view from y-axis down
+        view = (-180, 90, 2)
+        rotation = (-1., 0, 0, 'XYZ')
+    elif perspective == 'Z':
+        # view along z-axis
+        view = (180, 0, 2)
+        rotation = (1., 0, 0, 'XYZ')
+    elif perspective == 'X':
+        # view from X-axis down
+        view = (90, 0, 2)
+        rotation = (-np.pi/2, np.pi/2, 0.0, 'XYZ')
+
+    fig = ipv.pylab.gcf()
+    ipv.pylab.view(azimuth=view[0], elevation=view[1], distance=view[2])
+    fig.camera.rotation = rotation
+    
+    
+def my_volshow_1channel(volume, view=(0,0,0), rotation=(0,0,0, 'XYZ'), perspective=None, **kwargs):
+    """Customized volume show function, with specified camera position and angle.
+    
+    Args:
+        volume: volume data
+        view: camera position
+        rotation: camera angle
+    """
+        
+    ipv.quickvolshow(volume, **kwargs)
+    ipv.show()
+    
+    # wait for plot to be generated (JavaScript)
+    time.sleep(0.6)
+    
+    # set camera position and angle
+    set_perspective(view=view, rotation=rotation, perspective=perspective)
+    
+    
+def my_volshow_2channel(volume, view=(0,0,0), rotation=(0,0,0, 'XYZ'), perspective=None, 
+                        plot_params=None):
+    """Customized volume show function, with specified camera position and angle.
+    
+    Args:
+        volume: multi-channnel volume data
+        view: camera position
+        rotation: camera angle
+        perspective: alternatively, pre-saved camera perspective
+        plot_params: list of key-word arguments as a dictionary for ipv.volshow
+    """
+    
+    n_channels = len(volume)
+    if plot_params is None:
+        plot_params = [{}]*n_channels
+    assert len(plot_params) == n_channels 
+       
+    ipv.pylab.figure()
+    for ch in range(n_channels):
+        ipv.volshow(volume[ch, :], **plot_params[ch])
+    ipv.show()
+    
+    # wait for plot to be generated (JavaScript)
+    time.sleep(1)
+    
+    # set camera position and angle
+    set_perspective(view=view, rotation=rotation, perspective=perspective)
+    
+    
+def generate_3dmovies_1channel(stack, T=5, perspectives=['oblique', 'X', 'Y', 'Z'], name='', **kwargs):
+    """Generate 4 movies for each perspective
+    
+    Args:
+        stack: Input data
+        T: length of each movie in seconds
+    """
+    assert len(perspectives) > 0
+
+    # generate plot
+    my_volshow(stack[0,:], perspective=perspectives[0], **kwargs)
+
+    N_frames = len(stack)
+    fps = N_frames/T
+
+    # make 4 movies from 4 perspectives
+    for perspective in perspectives:
+        print(f'Generating {perspective} perspective...')
+
+        # set perspective
+        set_perspective(perspective=perspective)
+        time.sleep(1)
+
+        # define helper function
+        fig = ipv.pylab.gcf()
+        def go_through_volume(fig, i, fraction):
+            frame = np.round(fraction*N_frames).astype(np.int)
+            fig.volumes[0].data = stack[frame, :]
+
+        # create movie
+        tmpdir = ipv.pylab.movie(f'{name}{perspective}.gif', go_through_volume, fps=fps, frames=N_frames, endpoint=False)
+
+        # convert movie
+        #command = f'convert -delay 5.0 -loop 0 {tmpdir}/frame-*.png {perspective}.gif'
+        #stream = os.popen(command)
+        #output = stream.read()
+        
+        
+def generate_3dmovies_2channel(stack, T=5, perspectives=['oblique', 'X', 'Y', 'Z'], name='',
+                               plot_params=None):
+    """Generate 4 movies for each perspective
+    
+    Args:
+        stack: Input data
+        T: length of each movie in seconds
+        plot_params: list of key-word arguments as a dictionary for ipv.volshow
+    """
+    assert len(perspectives) > 0
+    
+    n_channels = stack.shape[1]
+    if plot_params is None:
+        plot_params = [{}]*n_channels
+    assert len(plot_params) == n_channels 
+
+    # generate plot
+    my_volshow_2channel(stack[0,:], perspective=perspectives[0], plot_params=plot_params)
+
+    N_frames = len(stack)
+    fps = N_frames/T
+
+    # make 4 movies from 4 perspectives
+    for perspective in perspectives:
+        print(f'Generating {perspective} perspective...')
+
+        # set perspective
+        set_perspective(perspective=perspective)
+        time.sleep(1)
+
+        # define helper function
+        fig = ipv.pylab.gcf()
+        def go_through_volume(fig, i, fraction):
+            frame = np.round(fraction*N_frames).astype(np.int)
+            for ch in range(n_channels):
+                fig.volumes[ch].data = stack[frame, ch, :]
+
+        # create movie
+        tmpdir = ipv.pylab.movie(f'{name}{perspective}.gif', go_through_volume, fps=fps, frames=N_frames, endpoint=False)
+
+        # convert movie
+        #command = f'convert -delay 5.0 -loop 0 {tmpdir}/frame-*.png {perspective}.gif'
+        #stream = os.popen(command)
+        #output = stream.read()
+        
+
+def make_box_for_grid(image_widget, title, size=(250,250)):
+    """
+    Make a VBox to hold caption/image for demonstrating
+    option_fit values.
+    """
+    modes = ['oblique', 'X', 'Y', 'Z']
+
+    # make layout
+    box_layout = Layout()
+    box_layout.width = f'{size[0]}px'
+    box_layout.height = f'{size[1]}px'
+    box_layout.border = '0px'
+
+    # Make the caption
+    if title is not None:
+        title_str = "'{}'".format(title)
+    else:
+        title_str = str(title)
+
+    h = HTML(value='' + str(title_str) + '')
+
+    # Make the box with the image widget inside it
+    boxb = Box()
+    boxb.layout = box_layout
+    boxb.children = [image_widget]
+
+    # Compose into a vertical box
+    vb = VBox()
+    vb.layout.align_items = 'center'
+    vb.children = [h, boxb]
+    
+    return vb
+
+
+def open_image(filename='oblique.gif', size=(250,250)):
+    
+    file = open(filename , "rb")
+    image = file.read()
+    progress = Image(
+        value=image,
+        format='gif',
+        width=size[0],
+        height=size[1])
+    return progress
+
+
+def show_movies(name=''):
+    
+    modes = ['oblique', 'X', 'Y', 'Z']
+
+    # define layout
+    hbox_layout = Layout()
+    hbox_layout.width = '100%'
+    #hbox_layout.justify_content = 'space-around'
+
+    # Use this margin to eliminate space between the image and the box
+    image_margin = '0 0 0 0'
+
+    # Set size of captions in figures below
+    caption_size = 'h4'
+    
+    # open GIF files
+    boxes = []
+    for mode in modes:
+        ib = open_image(filename=f'{name}{mode}.gif')
+        ib.layout.object_fit = 'contain'
+        ib.layout.margin = image_margin
+
+        boxes.append(make_box_for_grid(ib, title=mode))
+
+    # generate widgets
+    vb = HBox()
+    vb.layout.align_items = 'center'
+    hb = HBox()
+    hb.layout = hbox_layout
+    hb.children = boxes
+
+    vb.children = [hb]
+    
+    return vb
